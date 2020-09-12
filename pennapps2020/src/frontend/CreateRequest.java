d372af068ede49dfdcbf76f9b759af21101c8f5c
Katie Pizziketti <kpizziketti@gmail.com>
2020-09-12 13:06:04 -0400

    adding more buttons

--- pennapps2020/src/frontend/CreateRequest.java
+++ pennapps2020/src/frontend/CreateRequest.java
@@ -6,31 +6,49 @@ import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.*;
 
 public class CreateRequest extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
+		//set up the window and pane (grid for objects)
 		primaryStage.setTitle("STuINDER");
 		AnchorPane root = new AnchorPane();
 		
+		//create labels and buttons
 		Label selCorLabel = new Label("Select a course:");
-		Button c591 = new Button("591");
+		selCorLabel.setFont(Font.font("Arial", 30));
+		selCorLabel.setTextFill(Color.BLUE);
 		
+		Button c591 = new Button("591");
+		c591.setMinSize(25,20);
+		c591.setFont(Font.font("Arial", 16));
+		
+		Button c592 = new Button("592");
+		c592.setMinSize(25,20);
+		c592.setFont(Font.font("Arial", 16));
+		
+		
+		//set properties of labels and buttons
 		HBox header = new HBox(selCorLabel);
 		header.setMinSize(500, 100);
 		header.setAlignment(Pos.CENTER);
 		root.getChildren().add(header);
+		
 		HBox options = new HBox(c591);
+		options.getChildren().add(c592);
 		options.setAlignment(Pos.CENTER);
 		options.setMinSize(500, 200);
 		root.getChildren().add(options);
 		
+		//place labels and buttons in the pane
 		AnchorPane.setTopAnchor(header, 5.0);
-		AnchorPane.setBottomAnchor(options, 5.0);
+		AnchorPane.setBottomAnchor(options, 100.0);;
 		
-		
+		//add the pane to the scene
 		Scene scene = new Scene(root, 500, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
