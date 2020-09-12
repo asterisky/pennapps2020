c2345ef5d42a4908fec60345228a930f47175fe7
Katie Pizziketti <kpizziketti@gmail.com>
2020-09-12 13:53:11 -0400

    Button click now triggers second scene.

--- pennapps2020/src/frontend/CreateRequest.java
+++ pennapps2020/src/frontend/CreateRequest.java
@@ -5,16 +5,22 @@ import javafx.event.*;
 import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.*;
 
-public class CreateRequest extends Application{
+public class CreateRequest extends Application implements EventHandler{
+	
+	Stage window;
+	Scene selCourse, userInfo, selTimes;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		//set up the window and pane (grid for objects)
+		window = primaryStage;
+		
 		primaryStage.setTitle("STuINDER");
 		GridPane root = new GridPane();
 		root.setMinSize(500, 500);
@@ -22,14 +28,29 @@ public class CreateRequest extends Application{
 		root.setVgap(10);
 		root.setHgap(10);
 		
+		GridPane uInfo = new GridPane();
+		uInfo.setMinSize(500, 500);
+		uInfo.setPadding(new Insets(25,25,25,25));
+		uInfo.setVgap(10);
+		uInfo.setHgap(10);
+		
 		//create labels and buttons
 		Label selCorLabel = new Label("Select a course:");
 		selCorLabel.setFont(Font.font("Arial", 30));
 		selCorLabel.setTextFill(Color.BLUE);
 		
+		//event handler for button click
+		EventHandler<ActionEvent> buttonClick = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				window.setScene(userInfo);
+			}
+		};
+		
 		Button c591 = new Button("591");
 		c591.setMinSize(65,50);
 		c591.setFont(Font.font("Arial", 22));
+		c591.setOnAction(buttonClick);
 		
 		Button c592 = new Button("592");
 		c592.setMinSize(65,50);
@@ -51,6 +72,8 @@ public class CreateRequest extends Application{
 		c596.setMinSize(65,50);
 		c596.setFont(Font.font("Arial", 22));
 		
+
+		
 		
 		//set properties of labels and buttons
 		HBox header = new HBox(selCorLabel);
@@ -71,6 +94,8 @@ public class CreateRequest extends Application{
 		optionsBotRow.setAlignment(Pos.CENTER);
 		optionsBotRow.setMinSize(500, 100);
 		optionsBotRow.setSpacing(25);
+	
+		
 		
 		root.add(optionsTopRow, 1, 1);
 		root.add(optionsBotRow, 1, 2);
@@ -80,14 +105,16 @@ public class CreateRequest extends Application{
 
 		
 		//add the pane to the scene
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
+		selCourse = new Scene(root);
+		userInfo = new Scene(uInfo);
+		primaryStage.setScene(selCourse);
 		primaryStage.show();
 	}
 
+
+
 	public static void main(String[] args) {
 		launch(args);
-		System.out.println("Hello");
 	}
 
 }
