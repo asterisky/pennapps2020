e703386fedd76b72e98c4dd7baf787e6537e4718
HarunBul <57422346+HarunBul@users.noreply.github.com>
2020-09-12 23:50:50 -0400

    Update Shell.java

--- pennapps2020/src/data/Shell.java
+++ pennapps2020/src/data/Shell.java
@@ -1,10 +1,44 @@
 package data;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Class responsible for communicating with our virtual machine
- * 
- * @author Harun
  */
 public class Shell {
-    
+
+    // Extracts runtime associated with our app
+    Runtime runtime;
+
+    public Shell() {
+        runtime = Runtime.getRuntime();
+    }
+
+    // This website is useful for sending shell commands:
+    // https://stackabuse.com/executing-shell-commands-with-java/
+
+    public File getFile() {
+        File retFile = null;
+
+        String command = "";
+        try {
+            runtime.exec(command);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return retFile;
+    }
+
+    public void sendFile(File file) {
+        String command = "";
+        try {
+            runtime.exec(command);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
